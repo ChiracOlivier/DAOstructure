f096e607481772cc333e8bf895256bd80aeb6716
Chiracolivier <chiracolivier28@gmail.com>
2019-09-16 23:31:56 +0200

    import javabean in SuppresionClient.java

--- src/servletsClient/SuppressionClient.java
+++ src/servletsClient/SuppressionClient.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
+import javabean.Client;
 import com.sdzee.tp.dao.ClientDao;
 import com.sdzee.tp.dao.DAOException;
 import com.sdzee.tp.dao.DAOFactory;
